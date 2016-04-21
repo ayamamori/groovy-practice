3e763275127d8b9b61be6731e3399da9b1820add
yamamori.akihiro <yamamori.akihiro@donuts.ne.jp>
2016-04-21 18:54:02 +0900

    Added Generics.groovy

---
+++ src/Generics.groovy
@@ -0,0 +1,8 @@
+/**
+ * Created by yamamori.akihiro on 2016/04/21.
+ */
+
+List<String> strList = new ArrayList<>()
+strList += 3
+println(strList) // => [3]
+println(strList[0].class.name) // => java.lang.Integer

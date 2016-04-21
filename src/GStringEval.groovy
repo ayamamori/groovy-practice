d77442ff0e88744abfa47ac0faaaa2c1c6fb36e2
yamamori.akihiro <yamamori.akihiro@donuts.ne.jp>
2016-04-21 12:26:10 +0900

    Improved GStringEval

--- src/GStringEval.groovy
+++ src/GStringEval.groovy
@@ -4,14 +4,19 @@
 
 
 list = ["a"];
-gstring = "${list}" // => listは文字列変換されない
+gstring = "${list}" // => listはこの時点では文字列評価されない
 println(gstring) // => [a] このタイミングで文字列評価される
 list.add("b")
 println(gstring) // => [a,b] このタイミングで文字列評価される
 
 list = ["a"];
-gstring = "${list+'c'}" // => list+'c'は文字列変換される
+gstring = "${list+'c'}" // => list+'c'は文字列評価される
 println(gstring) // => [a,c]
 list.add("b")
-println(gstring) // => [a,c] gstring代入時に文字列変換済みなのでbにならない
+println(gstring) // => [a,c] gstring代入時に文字列に変換済みなのでbは入らない
 
+list = ["a"];
+String string = "${list}" // => GStringからStringへのキャストが発生し、文字列評価される
+println(string) // => [a]
+list.add("b")
+println(string) // => [a] 文字列評価済みなのでbは入らない
